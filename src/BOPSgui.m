fff6591a0206d5ae8d44b716fd5307284ba45d52
Alice Mantoan <alice.mantoan@dei.unipd.it>
2015-08-27 19:50:32 +0200

    implement Callback functions, 'Reset' and 'Default' buttons, add IK to 'Run'

--- src/BOPSgui.m
+++ src/BOPSgui.m
@@ -22,7 +22,7 @@ function varargout = BOPSgui(varargin)
 
 % Edit the above text to modify the response to help BOPSgui
 
-% Last Modified by GUIDE v2.5 24-Aug-2015 15:51:06
+% Last Modified by GUIDE v2.5 27-Aug-2015 19:08:19
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -124,11 +124,16 @@ function selectProcessingConfiguration_Callback(hObject, eventdata, handles)
 % hObject    handle to selectProcessingConfiguration (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+global inputDir model_file
+
 [inputDir, model_file]=processingConfiguration();
-handles.inputDir=inputDir;
-handles.model_file=model_file;
+%handles.inputDir=inputDir;
+%handles.model_file=model_file;
 set(handles.loadedInputFolder, 'String', inputDir);
 set(handles.lodedModel, 'String', model_file);
+
+
 
 % --- Executes on button press in CloseButton.
 function CloseButton_Callback(hObject, eventdata, handles)
@@ -137,11 +142,30 @@ function CloseButton_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 close(BOPSgui)
 
+
 % --- Executes on button press in RunButton.
 function RunButton_Callback(hObject, eventdata, handles)
 % hObject    handle to RunButton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+global selections inputDir model_file
+
+if selections.runIK == 1
+
+    [IKoutputDir, IKtrialsOutputDir, IKprocessedTrials]=InverseKinematics(inputDir, model_file); 
+end
+
+IKmotFilename='ik.mot'; %our default name
+
+set(handles.figure1, 'HandleVisibility', 'off');
+if selections.plotIK == 1
+    [coordinates,Xaxislabel]=plotResults('IK', IKoutputDir, IKtrialsOutputDir, model_file, IKprocessedTrials, IKmotFilename);
+end
+set(handles.figure1, 'HandleVisibility', 'on');
+
+
+
 
 
 % --- Executes on button press in ResetButton.
@@ -150,6 +174,30 @@ function ResetButton_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+set(handles.runIK,'Value',0);
+set(handles.PlotResultsIK,'Value',0);
+set(handles.PlotResultsIK, 'Enable', 'off')
+
+set(handles.runMA,'Value',0);
+set(handles.plotStorageMA,'Value',0);
+set(handles.plotStorageMA, 'Enable', 'off')
+set(handles.selectTrialsMA,'Value',0);
+set(handles.selectXaxisMA,'Value',0);
+
+set(handles.runID,'Value',0);
+set(handles.plotResultsID,'Value',0);
+set(handles.plotResultsID, 'Enable', 'off')
+set(handles.selectTrialsID,'Value',0);
+set(handles.selectCoordinatesToPlotID,'Value',0);
+
+set(handles.runSO,'Value',0);
+set(handles.plotStorageSO,'Value',0);
+set(handles.plotStorageSO, 'Enable', 'off')
+set(handles.selectTrialsSO,'Value',0);
+set(handles.selectXaxisSO,'Value',0);
+
+
+
 
 % --- Executes on button press in setDefault.
 function setDefault_Callback(hObject, eventdata, handles)
@@ -157,13 +205,59 @@ function setDefault_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+set(handles.runIK,'Value',1);
+set(handles.PlotResultsIK, 'Enable', 'on')
+set(handles.PlotResultsIK,'Value',1);
 
-% --- If Enable == 'on', executes on mouse press in 5 pixel border.
-% --- Otherwise, executes on mouse press in 5 pixel border or over runIK.
-function runIK_ButtonDownFcn(hObject, eventdata, handles)
+set(handles.runMA,'Value',1);
+set(handles.plotStorageMA, 'Enable', 'on')
+set(handles.plotStorageMA,'Value',1);
+
+set(handles.runID,'Value',1);
+set(handles.plotResultsID, 'Enable', 'on')
+set(handles.plotResultsID,'Value',1);
+set(handles.selectTrialsID,'Value',1);
+
+set(handles.runSO,'Value',1);
+set(handles.plotStorageSO, 'Enable', 'on')
+set(handles.plotStorageSO,'Value',1);
+
+
+
+% --- Executes on button press in runIK.
+function runIK_Callback(hObject, eventdata, handles)
 % hObject    handle to runIK (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of runIK
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.runIK = 1;
+    set(handles.PlotResultsIK, 'Enable', 'on')
+else
+	selections.runIK =0;
+    set(handles.PlotResultsIK, 'Enable', 'off')
+end
+
+
+% --- Executes on button press in PlotResultsIK.
+function PlotResultsIK_Callback(hObject, eventdata, handles)
+% hObject    handle to PlotResultsIK (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of PlotResultsIK
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.plotIK = 1;
+else
+	selections.plotIK =0;
+end
 
 
 
@@ -174,51 +268,207 @@ function runMA_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of runMA
+global selections
+    
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.runMA = 1;
+    set(handles.plotStorageMA, 'Enable', 'on')
+else
+	selections.runMA =0;
+    set(handles.plotStorageMA, 'Enable', 'off')
+end
 
 
-% --- If Enable == 'on', executes on mouse press in 5 pixel border.
-% --- Otherwise, executes on mouse press in 5 pixel border or over runMA.
-function runMA_ButtonDownFcn(hObject, eventdata, handles)
-% hObject    handle to runMA (see GCBO)
+% --- Executes on button press in plotStorageMA.
+function plotStorageMA_Callback(hObject, eventdata, handles)
+% hObject    handle to plotStorageMA (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+% Hint: get(hObject,'Value') returns toggle state of plotStorageMA
+global selections
 
-% --- Executes on key press with focus on runMA and none of its controls.
-function runMA_KeyPressFcn(hObject, eventdata, handles)
-% hObject    handle to runMA (see GCBO)
-% eventdata  structure with the following fields (see UICONTROL)
-%	Key: name of the key that was pressed, in lower case
-%	Character: character interpretation of the key(s) that was pressed
-%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --- Executes on key press with focus on runIK and none of its controls.
-function runIK_KeyPressFcn(hObject, eventdata, handles)
-% hObject    handle to runIK (see GCBO)
-% eventdata  structure with the following fields (see UICONTROL)
-%	Key: name of the key that was pressed, in lower case
-%	Character: character interpretation of the key(s) that was pressed
-%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
-% handles    structure with handles and user data (see GUIDATA)
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.plotMA = 1;
+else
+	selections.plotMA =0;
+end
 
 
 
-
-% --- If Enable == 'on', executes on mouse press in 5 pixel border.
-% --- Otherwise, executes on mouse press in 5 pixel border or over PlotResultsIK.
-function PlotResultsIK_ButtonDownFcn(hObject, eventdata, handles)
-% hObject    handle to PlotResultsIK (see GCBO)
+% --- Executes on button press in runID.
+function runID_Callback(hObject, eventdata, handles)
+% hObject    handle to runID (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+% Hint: get(hObject,'Value') returns toggle state of runID
 
-% --- Executes on key press with focus on PlotResultsIK and none of its controls.
-function PlotResultsIK_KeyPressFcn(hObject, eventdata, handles)
-% hObject    handle to PlotResultsIK (see GCBO)
-% eventdata  structure with the following fields (see UICONTROL)
-%	Key: name of the key that was pressed, in lower case
-%	Character: character interpretation of the key(s) that was pressed
-%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.runID = 1;
+    set(handles.plotResultsID, 'Enable', 'on')
+else
+	selections.runID =0;
+    set(handles.plotResultsID, 'Enable', 'off')
+end
+
+
+% --- Executes on button press in plotResultsID.
+function plotResultsID_Callback(hObject, eventdata, handles)
+% hObject    handle to plotResultsID (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of plotResultsID
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.plotID = 1;
+else
+	selections.plotID =0;
+end
+
+
+
+% --- Executes on button press in runSO.
+function runSO_Callback(hObject, eventdata, handles)
+% hObject    handle to runSO (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of runSO
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.runSO = 1;
+    set(handles.plotStorageSO, 'Enable', 'on')
+else
+	selections.runSO =0;
+    set(handles.plotStorageSO, 'Enable', 'off')
+end
+
+
+% --- Executes on button press in plotStorageSO.
+function plotStorageSO_Callback(hObject, eventdata, handles)
+% hObject    handle to plotStorageSO (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of plotStorageSO
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.plotSO = 1;
+else
+	selections.plotSO =0;
+end
+
+
+
+% --- Executes on button press in selectTrialsMA.
+function selectTrialsMA_Callback(hObject, eventdata, handles)
+% hObject    handle to selectTrialsMA (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of selectTrialsMA
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.selectTrialsMA = 1;
+else
+	selections.selectTrialsMA =0;
+end
+
+
+% --- Executes on button press in selectXaxisMA.
+function selectXaxisMA_Callback(hObject, eventdata, handles)
+% hObject    handle to selectXaxisMA (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of selectXaxisMA
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.selectXaxisMA = 1;
+else
+	selections.selectXaxisMA =0;
+end
+
+
+
+% --- Executes on button press in selectTrialsID.
+function selectTrialsID_Callback(hObject, eventdata, handles)
+% hObject    handle to selectTrialsID (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of selectTrialsID
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.selectTrialsID = 1;
+else
+	selections.selectTrialsID =0;
+end
+
+
+
+% --- Executes on button press in selectCoordinatesToPlotID.
+function selectCoordinatesToPlotID_Callback(hObject, eventdata, handles)
+% hObject    handle to selectCoordinatesToPlotID (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of selectCoordinatesToPlotID
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.selectCoordinatesToPlotID = 1;
+else
+	selections.selectCoordinatesToPlotID =0;
+end
+
+
+
+% --- Executes on button press in selectTrialsSO.
+function selectTrialsSO_Callback(hObject, eventdata, handles)
+% hObject    handle to selectTrialsSO (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of selectTrialsSO
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.selectTrialsSO = 1;
+else
+	selections.selectTrialsSO =0;
+end
+
+
+% --- Executes on button press in selectXaxisSO.
+function selectXaxisSO_Callback(hObject, eventdata, handles)
+% hObject    handle to selectXaxisSO (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of selectXaxisSO
+
+global selections
+
+if (get(hObject,'Value') == get(hObject,'Max'))
+	selections.selectXaxisSO = 1;
+else
+	selections.selectXaxisSO =0;
+end
